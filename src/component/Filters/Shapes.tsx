bd6496e89c7620a13e770cc0e49a8a1870318026
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-06-06 10:56:48 +0100

    Shapes component refactored

--- src/component/Filters/Shapes.tsx
+++ src/component/Filters/Shapes.tsx
@@ -1,39 +1,16 @@
+const Shapes_List = ['oval', 'round', 'triangle', 'square', 'rectangle'];
 
 const Shapes = () => {
   return (
     <section className="shapes">
       <h4 className="shapes-title">Shapes</h4>
       <ul className="shapes-type">
-        <li>
-          <input type="checkbox" name="oval" id="oval" />
-          <label htmlFor="oval">
-            Oval
-          </label>
-        </li>
-        <li>
-          <input type="checkbox" name="round" id="round" />
-          <label htmlFor="round">
-            Round
-          </label>
-        </li>
-        <li>
-          <input type="checkbox" name="triangle" id="triangle" />
-          <label htmlFor="triangle">
-            Triangle
-          </label>
-        </li>
-        <li>
-          <input type="checkbox" name="square" id="square" />
-          <label htmlFor="square">
-            Square
-          </label>
-        </li>
-        <li>
-          <input type="checkbox" name="rectangle" id="rectangle" />
-          <label htmlFor="rectangle">
-            Rectangle
-          </label>
-        </li>
+        {Shapes_List.map((shape, index) => (
+          <li key={index}>
+            <input type="checkbox" name={shape} id={shape} />
+            <label htmlFor={shape} className={shape}>{shape}</label>
+          </li>
+        ))}
       </ul>
     </section>
   )
