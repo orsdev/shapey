f85b46fe0dd94c4d4255e8e6072c2896e11240e6
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-06-06 10:26:41 +0100

    Filter component added

---
+++ src/component/Filters/Filters.tsx
@@ -0,0 +1,12 @@
+import Shapes from "./Shapes"
+
+const Filters = () => {
+  return (
+    <section className="filters">
+      <h3 className="filters-title">Filters</h3>
+      <Shapes />
+    </section>
+  )
+}
+
+export default Filters;
