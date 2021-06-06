cc47a38cfd717686de7af2c7888cb8b816dd3ab6
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-06-06 10:53:38 +0100

    Colors component refactored

--- src/component/Filters/Colors.tsx
+++ src/component/Filters/Colors.tsx
@@ -1,34 +1,20 @@
 import React from 'react'
 
+const Colors_List = [
+  'red', 'blue', 'green', 'yellow', 'skyblue', 'gray'
+];
+
 const Colors = () => {
   return (
     <section className="colors">
       <h4 className="colors-title">Colors</h4>
       <ul className="colors-type">
-        <li>
-          <input type="checkbox" name="red" id="red" />
-          <label htmlFor="red" className="red" />
-        </li>
-        <li>
-          <input type="checkbox" name="blue" id="blue" />
-          <label htmlFor="blue" className="blue" />
-        </li>
-        <li>
-          <input type="checkbox" name="green" id="green" />
-          <label htmlFor="green" className="green" />
-        </li>
-        <li>
-          <input type="checkbox" name="yellow" id="yellow" />
-          <label htmlFor="yellow" className="yellow" />
-        </li>
-        <li>
-          <input type="checkbox" name="skyblue" id="skyblue" />
-          <label htmlFor="skyblue" className="skyblue" />
-        </li>
-        <li>
-          <input type="checkbox" name="gray" id="gray" />
-          <label htmlFor="gray" className="gray" />
-        </li>
+        {Colors_List.map((color, index) => (
+          <li key={index}>
+            <input type="checkbox" name={color} id={color} />
+            <label htmlFor={color} className={color} />
+          </li>
+        ))}
       </ul>
     </section>
   )
