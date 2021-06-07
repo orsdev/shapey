0ccc11d11995d15f50acb3db2f5184ea9739031b
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-06-07 16:02:29 +0100

    colors redux state used in component

--- src/component/ShapesColorsGrid.tsx
+++ src/component/ShapesColorsGrid.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addShapes } from '../redux/actions/shapes.action';
+import { addColors } from '../redux/actions/colors.action';
 import { RootState } from '../redux/rootReducer';
 import Data from '../data.json';
 
@@ -8,22 +9,27 @@ const ShapesColorsGrid: React.FC = () => {
   const { shapes } = useSelector(
     (state: RootState) => state.shapes);
 
+  const { colors } = useSelector(
+    (state: RootState) => state.colors);
   const dispatch = useDispatch();
 
   React.useEffect(() => {
     dispatch(addShapes(Data.shapes));
+    dispatch(addColors(Data.colors));
   }, [])
 
   return (
     <section className="grid">
-      <h4 className="grid-title">Title</h4>
+      <h4 className="grid-title">All items</h4>
       <ul className="grid-items">
-        {shapes.map((value: string, index: number) => {
-          return (
-            <li key={index}>
-              <span className="oval red" />
-            </li>
-          )
+        {shapes.map((shape: string) => {
+          return colors.map((color: string, index: number) => {
+            return (
+              <li key={index}>
+                <span className={`${shape} ${color}`} />
+              </li>
+            )
+          })
         })}
       </ul>
     </section>
