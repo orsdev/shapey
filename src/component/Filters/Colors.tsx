79e97d3b958d7a1107209fbb4075a168d750fc99
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-06-07 17:46:40 +0100

    add & remove color features added

--- src/component/Filters/Colors.tsx
+++ src/component/Filters/Colors.tsx
@@ -1,17 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { RootState } from '../../redux/rootReducer';
+import { useDispatch, useSelector } from 'react-redux';
+import { addColors, removeColors } from '../../redux/actions/colors.action';
+import Data from '../../data.json';
 
 const Colors_List = [
   'red', 'blue', 'green', 'yellow', 'skyblue', 'gray'
 ];
 
 const Colors = () => {
+  const { colors } = useSelector(
+    (state: RootState) => state.colors);
+
+  const dispatch = useDispatch();
+  const [checkedState, setCheckedState] = React.useState(
+    new Array(Data.colors.length).fill(true)
+  );
+
+  React.useEffect(() => {
+    if (!colors.length) {
+      dispatch(addColors(Data.colors));
+      setCheckedState(new Array(Data.colors.length).fill(true))
+    }
+  }, [colors.length]);
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>, position: number) => {
+    const { target } = e;
+
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+
+    setCheckedState(updatedCheckedState);
+
+    if (target.checked) {
+      dispatch(addColors(target.name))
+    } else {
+      dispatch(removeColors(target.name))
+    }
+  }
+
   return (
     <section className="colors">
       <h4 className="colors-title">Colors</h4>
       <ul className="colors-type">
         {Colors_List.map((color, index) => (
           <li key={index}>
-            <input type="checkbox" name={color} id={color} />
+            <input type="checkbox" name={color} id={color} checked={checkedState[index]} onChange={(e) => onChangeHandler(e, index)} />
             <label htmlFor={color} className={color} />
           </li>
         ))}
