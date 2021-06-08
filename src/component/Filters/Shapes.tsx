909e3fc06e3d2689180d4db887ac3a204fed24c7
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-06-08 19:48:26 +0100

    addColors function dispatch when colors in empty

--- src/component/Filters/Shapes.tsx
+++ src/component/Filters/Shapes.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { RootState } from '../../redux/rootReducer';
 import { useDispatch, useSelector } from 'react-redux';
+import { addColors } from '../../redux/actions/colors.action';
 import { addShapes, removeShapes } from '../../redux/actions/shapes.action';
 import Data from '../../data.json';
 
 const Shapes = () => {
   const { shapes } = useSelector(
     (state: RootState) => state.shapes);
+
+  const { colors } = useSelector(
+    (state: RootState) => state.colors);
 
   const dispatch = useDispatch();
   const [checkedState, setCheckedState] = React.useState(
@@ -16,9 +20,16 @@ const Shapes = () => {
   React.useEffect(() => {
     if (!shapes.length) {
       dispatch(addShapes(Data.shapes));
+      dispatch(addColors(Data.colors));
       setCheckedState(new Array(Data.shapes.length).fill(true))
     }
   }, [shapes.length]);
+
+  React.useEffect(() => {
+    if (!colors.length) {
+      setCheckedState(new Array(Data.shapes.length).fill(true))
+    }
+  }, [colors.length]);
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>, position: number) => {
     const { target } = e;
