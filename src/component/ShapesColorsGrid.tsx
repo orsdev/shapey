a11b22c428dd437f721cd66c2a48e83534673072
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-06-08 19:48:59 +0100

    title feature added

--- src/component/ShapesColorsGrid.tsx
+++ src/component/ShapesColorsGrid.tsx
@@ -5,6 +5,44 @@ import { addColors } from '../redux/actions/colors.action';
 import { RootState } from '../redux/rootReducer';
 import Data from '../data.json';
 
+const allSelectedItems = (items: string[], type: string): boolean => {
+
+  if (type === 'colors') {
+    return Data.colors.every((color) => {
+      return items.includes(color);
+    })
+  } else {
+    return Data.shapes.every((shape) => {
+      return items.includes(shape);
+    })
+  }
+}
+
+
+const updateTitle = (shapes: string[], colors: string[]): string => {
+  let title;
+
+  if (allSelectedItems(colors, 'colors') && allSelectedItems(shapes, 'shapes')) {
+    title = 'All Items';
+  } else if (shapes.length === 1 && colors.length === 1) {
+    title = `${shapes.join("")} ${colors.join("")} Item`;
+  } else if (allSelectedItems(colors, 'colors') && shapes.length === 1) {
+    title = `All ${shapes.join("")} Items`;
+  } else if (allSelectedItems(shapes, 'shapes') && colors.length === 1) {
+    title = `All ${colors.join("")} Items`;
+  } else if (allSelectedItems(colors, 'colors') && shapes.length > 1
+    || allSelectedItems(shapes, 'shapes') && colors.length > 1) {
+    title = `Multiple Items`;
+  } else if (shapes.length === 1 && colors.length > 1) {
+    title = `Multiple ${shapes.join("")} Items`
+  } else if (shapes.length > 1 && colors.length === 1) {
+    title = `Multiple ${colors.join("")} Items`
+  } else {
+    title = 'Multiple Items';
+  }
+  return title;
+}
+
 const ShapesColorsGrid: React.FC = () => {
   const { shapes } = useSelector(
     (state: RootState) => state.shapes);
@@ -13,14 +51,25 @@ const ShapesColorsGrid: React.FC = () => {
     (state: RootState) => state.colors);
   const dispatch = useDispatch();
 
+  const [title, setTitle] = React.useState('All Items');
+
   React.useEffect(() => {
     dispatch(addShapes(Data.shapes));
     dispatch(addColors(Data.colors));
   }, [])
 
+  React.useEffect(() => {
+    if (shapes.length || colors.length) {
+      const updatedTitle = updateTitle(shapes, colors);
+      setTitle(updatedTitle);
+    }
+
+  }, [shapes.length, colors.length])
+
+
   return (
     <section className="grid">
-      <h4 className="grid-title">All items</h4>
+      <h4 className="grid-title">{title}</h4>
       <ul className="grid-items">
         {shapes.map((shape: string) => {
           return colors.map((color: string, index: number) => {
